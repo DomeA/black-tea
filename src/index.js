5e72b702280552fff0759d201e44d823719b5772
DomeA <domeastudio@hotmail.com>
2020-05-19 11:57:43 +0800

    完成 测试框架集成

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+let moduleExports = {};
+const r = require.context('./', true, /^\.\/.+\/.+\.js$/);
+r.keys().forEach(key => {
+    let attr = key.substring(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
+    moduleExports[attr] = r(key);
+});
+module.exports = moduleExports;
